aec302e3150403b1dd34e9328a136198d756b4a4
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-05-25 15:29:12 +0530

    Update npm dependencies and add NextAuthProviders component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { NextAuthProviders } from "./Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProviders>
+        {children}
+        </NextAuthProviders>
+        
+        </body>
     </html>
   );
 }
